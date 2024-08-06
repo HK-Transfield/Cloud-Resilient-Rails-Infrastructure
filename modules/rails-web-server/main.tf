2bc628eccf64e945705b02068a22ff88cdb4dab2
HK Transfield <hktransfield.dev@gmail.com>
2024-08-06 16:13:02 +1200

    refactor: remove provisioners

--- modules/rails-web-server/main.tf
+++ modules/rails-web-server/main.tf
@@ -1,23 +1,16 @@
+/**
+Name: Ruby of Rails EC2 Instance
+Contributors: HK Transfield
+*/
+
 resource "aws_instance" "webserver" {
   ami           = "ami-0427090fd1714168b"
   instance_type = "t2.micro"
-
-  provisioner "file" {
-    source      = var.script_filename
-    destination = var.script_filename
-  }
-
-  # https://docs.aws.amazon.com/elasticbeanstalk/latest/dg/ruby-development-environment.html
-  # https://rvm.io/
-  # https://www.ruby-lang.org/en/documentation/installation/
-  provisioner "remote-exec" {
-    inline = [
-      "chmod +x ${var.script_filename}",
-      "./${var.script_filename}"
-    ]
-  }
+  key_name      = var.key_name
 
   tags = {
     Name = "${var.name_prefix}-web-server"
   }
+
+  user_data = file("${path.module}/script.sh")
 }
