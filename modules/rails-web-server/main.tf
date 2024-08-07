c55960463ff3dbab8d53b2d465e4ffa759258fe7
HK Transfield <hktransfield.dev@gmail.com>
2024-08-08 10:55:08 +1200

    feat: add cloudinit_config to help provision instance

--- modules/rails-web-server/main.tf
+++ modules/rails-web-server/main.tf
@@ -3,14 +3,34 @@ Name: Ruby of Rails EC2 Instance
 Contributors: HK Transfield
 */
 
+locals {
+  filename = "script.sh"
+  filepath = "${path.module}/${local.filename}"
+}
+
+data "cloudinit_config" "user_data" {
+  gzip          = false
+  base64_encode = false
+
+  part {
+    filename     = local.filename
+    content_type = "text/x-shellscript"
+
+    content = file(local.filepath)
+  }
+}
+
+#TODO: https://developer.hashicorp.com/terraform/language/resources/provisioners/syntax
+#todo: https://registry.terraform.io/providers/hashicorp/cloudinit/latest/docs/data-sources/config
 resource "aws_instance" "webserver" {
-  ami           = "ami-0427090fd1714168b"
-  instance_type = "t2.micro"
-  key_name      = var.key_name
+  ami             = "ami-0ae8f15ae66fe8cda"
+  instance_type   = "t2.micro"
+  key_name        = var.key_name
+  security_groups = ["launch-wizard-1"]
 
   tags = {
     Name = "${var.name_prefix}-web-server"
   }
 
-  user_data = file("${path.module}/script.sh")
+  user_data = data.cloudinit_config.user_data.rendered
 }
