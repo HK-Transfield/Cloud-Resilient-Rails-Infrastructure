ffcc23955f9f6576682d3d6c182d082519b11324
HK Transfield <hktransfield.dev@gmail.com>
2024-09-04 10:17:33 +1200

    docs: improve heading comments

--- modules/rails-web-server/variables.tf
+++ modules/rails-web-server/variables.tf
@@ -4,7 +4,31 @@ variable "name_prefix" {
   default     = "rails"
 }
 
+################################################################################
+# EC2 Settings
+################################################################################
+
 variable "key_name" {
   description = "The name of an Amazon EC2 key pair."
+  type        = string
+}
+
+variable "instance_type" {
+  description = "The type of EC2 instance."
+  type        = string
+  default     = "t2.micro"
+}
+
+################################################################################
+# Network Settings
+################################################################################
+
+variable "vpc_id" {
+  description = "The VPC to launch the EC2 instance in."
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "The VPC subnet ID to launch in."
   type        = string
 }
