12b2c47d03c7b354e3743d2a096aa5bd4374bfe6
HK Transfield <hktransfield.dev@gmail.com>
2024-09-04 10:16:21 +1200

    feat: add ec2 security groups

--- modules/rails-web-server/main.tf
+++ modules/rails-web-server/main.tf
@@ -3,15 +3,29 @@ Name: Ruby of Rails EC2 Instance
 Contributors: HK Transfield
 */
 
-locals {
-  filename = "install_rails.sh"
-  filepath = "${path.module}/scripts/${local.filename}"
+################################################################################
+# Amazon Machine Image (AMI)
+################################################################################
+
+data "aws_ami" "amazon_linux" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["al2023-ami-*-kernel-6.1-x86_64"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
 }
 
-data "cloudinit_config" "user_data" {
-  gzip          = false
-  base64_encode = false
+################################################################################
+# EC2 User Data
+################################################################################
 
+data "cloudinit_config" "user_data" {
   part {
     filename     = local.filename
     content_type = "text/x-shellscript"
@@ -20,17 +34,58 @@ data "cloudinit_config" "user_data" {
   }
 }
 
-#TODO: https://developer.hashicorp.com/terraform/language/resources/provisioners/syntax
-#todo: https://registry.terraform.io/providers/hashicorp/cloudinit/latest/docs/data-sources/config
+locals {
+  filename = "install_rails_yum.sh"
+  filepath = "${path.module}/scripts/${local.filename}"
+}
+
+################################################################################
+# EC2 Web Server
+################################################################################
+
 resource "aws_instance" "webserver" {
-  ami             = "ami-0a0e5d9c7acc336f1"
-  instance_type   = "t2.micro"
-  key_name        = var.key_name
-  security_groups = ["launch-wizard-1"]
+  ami                    = data.aws_ami.amazon_linux.id
+  instance_type          = var.instance_type
+  key_name               = var.key_name
+  subnet_id              = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.webserver.id]
 
   tags = {
-    Name = "${var.name_prefix}-web-server"
+    Name    = "${var.name_prefix}-web-server"
+    Project = var.name_prefix
   }
 
   user_data = data.cloudinit_config.user_data.rendered
+}
+
+################################################################################
+# VPC Security Group
+################################################################################
+
+resource "aws_security_group" "webserver" {
+  name        = "${var.name_prefix}-sg"
+  description = "Security Group for ${var.name_prefix} Web Server"
+  vpc_id      = var.vpc_id
+
+  tags = {
+    Name    = "${var.name_prefix}-sg"
+    Project = var.name_prefix
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "name" {
+  security_group_id = aws_security_group.webserver.id
+  description       = "SSH from anywhere"
+  from_port         = 22
+  to_port           = 22
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_egress_rule" "name" {
+  security_group_id = aws_security_group.webserver.id
+  from_port         = 0
+  to_port           = 0
+  ip_protocol       = "-1" # Allows all outbound traffic
+  cidr_ipv4         = "0.0.0.0/0"
 }
