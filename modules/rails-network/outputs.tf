dab080cd7455b7b7877a2234aa0e8f6df19652e3
HK Transfield <hktransfield.dev@gmail.com>
2024-09-11 09:08:53 +1200

    feat: add outputs for all subnet IDs in both AZs

--- modules/rails-network/outputs.tf
+++ modules/rails-network/outputs.tf
@@ -1,6 +1,14 @@
+################################################################################
+# VPC
+################################################################################
+
 output "vpc_id" {
   value = aws_vpc.this.id
 }
+
+################################################################################
+# Public Subnets
+################################################################################
 
 output "web_a_subnet_id" {
   value = aws_subnet.web["A"].id
@@ -10,3 +18,22 @@ output "web_b_subnet_id" {
   value = aws_subnet.web["B"].id
 }
 
+################################################################################
+# Private Subnets
+################################################################################
+
+output "app_a_subnet_id" {
+  value = aws_subnet.app["A"].id
+}
+
+output "app_b_subnet_id" {
+  value = aws_subnet.app["B"].id
+}
+
+output "db_a_subnet_id" {
+  value = aws_subnet.db["A"].id
+}
+
+output "db_b_subnet_id" {
+  value = aws_subnet.db["B"].id
+}
