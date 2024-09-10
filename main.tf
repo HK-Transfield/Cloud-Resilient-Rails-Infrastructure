c46533282a2f96347081f714d9a12474ae844f37
HK Transfield <hktransfield.dev@gmail.com>
2024-09-11 09:04:28 +1200

    refactor: pass in az from parent to child module

--- main.tf
+++ main.tf
@@ -1,88 +1,84 @@
 /**
-Name: Ruby on Rails AWS Web Server
+Name: Cloud Resilient Ruby-on-Rails Infrastructure
 Contributors: HK Transfield
 */
 
+data "aws_availability_zones" "available" {}
+
 ################################################################################
-# Rails EC2 Instance
+# Network Infrastructure
+################################################################################
+
+locals {
+  az_a = data.aws_availability_zones.available.names[0]
+  az_b = data.aws_availability_zones.available.names[1]
+}
+
+module "rails-network" {
+  source     = "./modules/rails-network"
+  cidr_block = "10.17.0.0/16"
+  db_subnet_cidrs = {
+    "A" = {
+      cidr_block             = "10.17.16.0/20"
+      ipv6_cidr_block_netnum = 1
+      availability_zone      = local.az_a
+    }
+    "B" = {
+      cidr_block             = "10.17.80.0/20"
+      ipv6_cidr_block_netnum = 5
+      availability_zone      = local.az_b
+    }
+  }
+  app_subnet_cidrs = {
+    "A" = {
+      cidr_block             = "10.17.32.0/20"
+      ipv6_cidr_block_netnum = 2
+      availability_zone      = local.az_a
+    }
+    "B" = {
+      cidr_block             = "10.17.48.0/20"
+      ipv6_cidr_block_netnum = 6
+      availability_zone      = local.az_b
+    }
+  }
+  web_subnet_cidrs = {
+    "A" = {
+      cidr_block             = "10.17.96.0/20"
+      ipv6_cidr_block_netnum = 3
+      availability_zone      = local.az_a
+    }
+    "B" = {
+      cidr_block             = "10.17.112.0/20"
+      ipv6_cidr_block_netnum = 7
+      availability_zone      = local.az_b
+    }
+  }
+}
+
+################################################################################
+# Application Server Instance
 ################################################################################
 
 data "aws_key_pair" "this" {
   key_name = "rails-key"
 }
 
-module "rails-web-server" {
-  source    = "./modules/rails-web-server"
+module "rails-app-server-A" {
+  source    = "./modules/rails-app-server"
   key_name  = data.aws_key_pair.this.key_name
   vpc_id    = module.rails-network.vpc_id
-  subnet_id = module.rails-network.web_a_subnet_id
+  subnet_id = module.rails-network.app_a_subnet_id
+}
+
+module "rails-app-server-B" {
+  source    = "./modules/rails-app-server"
+  key_name  = data.aws_key_pair.this.key_name
+  vpc_id    = module.rails-network.vpc_id
+  subnet_id = module.rails-network.app_b_subnet_id
 }
 
 ################################################################################
-# Rails Network Architecture
+# Rails Database Instance
 ################################################################################
 
-locals {
-  cidr_block = "10.17.0.0/16"
-
-  reserved = {
-    "A" = {
-      cidr_block             = "10.17.0.0/20"
-      ipv6_cidr_block_netnum = 0
-      az_name_index          = 0
-    }
-    "B" = {
-      cidr_block             = "10.17.64.0/20"
-      ipv6_cidr_block_netnum = 4
-      az_name_index          = 1
-    }
-  }
-
-  db = {
-    "A" = {
-      cidr_block             = "10.17.16.0/20"
-      ipv6_cidr_block_netnum = 1
-      az_name_index          = 0
-    }
-    "B" = {
-      cidr_block             = "10.17.80.0/20"
-      ipv6_cidr_block_netnum = 5
-      az_name_index          = 1
-    }
-  }
-
-  app = {
-    "A" = {
-      cidr_block             = "10.17.32.0/20"
-      ipv6_cidr_block_netnum = 2
-      az_name_index          = 0
-    }
-    "B" = {
-      cidr_block             = "10.17.48.0/20"
-      ipv6_cidr_block_netnum = 6
-      az_name_index          = 1
-    }
-  }
-
-  web = {
-    "A" = {
-      cidr_block             = "10.17.96.0/20"
-      ipv6_cidr_block_netnum = 3
-      az_name_index          = 0
-    }
-    "B" = {
-      cidr_block             = "10.17.112.0/20"
-      ipv6_cidr_block_netnum = 7
-      az_name_index          = 1
-    }
-  }
-}
-
-module "rails-network" {
-  source                = "./modules/rails-network"
-  cidr_block            = local.cidr_block
-  reserved_subnet_cidrs = local.reserved
-  db_subnet_cidrs       = local.db
-  app_subnet_cidrs      = local.app
-  web_subnet_cidrs      = local.web
-}
+#TODO
