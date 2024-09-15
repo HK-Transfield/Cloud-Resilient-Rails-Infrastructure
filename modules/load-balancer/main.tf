8a539d151054a8c570f5267f19bd08c0c289ff62
HK Transfield <hktransfield.dev@gmail.com>
2024-09-16 10:39:54 +1200

    feat: add s3 bucket to store logs

--- modules/load-balancer/main.tf
+++ modules/load-balancer/main.tf
@@ -1,6 +1,6 @@
 /**
 Name: Cloud Resilient Application Load Balancing Module
-Contributors: HK Transfield, 2024
+Author: HK Transfield, 2024
 
 Provides a simple internet facing Application Load Balancer (ALB). The 
 ALB will automatically distribute traffic across the EC2 instances in 
@@ -9,6 +9,51 @@ multiple Availability Zones.
 This module is designed to be used as part of the Cloud Resilient Rails 
 Infrastructure project.
 */
+
+################################################################################
+# Log Storage Configuration
+################################################################################
+
+data "aws_elb_service_account" "main" {}
+
+locals {
+  bucket_name = "${local.alb_name}-logs-${random_id.this.hex}"
+}
+
+resource "random_id" "this" {
+  byte_length = 8
+}
+
+resource "aws_s3_bucket" "this" {
+  force_destroy = true # To destroy the alb log prefixes
+  bucket        = local.bucket_name
+
+  tags = {
+    project = var.project_name
+  }
+}
+
+resource "aws_s3_bucket_policy" "this" {
+  bucket = aws_s3_bucket.this.id
+  policy = data.aws_iam_policy_document.s3_bucket_lb_write.json
+}
+
+data "aws_iam_policy_document" "s3_bucket_lb_write" {
+  policy_id = "s3_bucket_lb_logs"
+
+  statement {
+    actions = [
+      "s3:PutObject"
+    ]
+    effect    = "Allow"
+    resources = ["${aws_s3_bucket.this.arn}/*"]
+
+    principals {
+      type        = "AWS"
+      identifiers = ["${data.aws_elb_service_account.main.arn}"]
+    }
+  }
+}
 
 ################################################################################
 # Application Load Balancer
@@ -22,19 +67,23 @@ resource "aws_lb" "this" {
   name               = local.alb_name
   internal           = false # Make it internet facing
   load_balancer_type = "application"
-  security_groups    = [aws_security_group.alb_sg.id]
+  security_groups    = [aws_security_group.this.id]
   subnets            = var.subnets
 
   access_logs {
-    bucket  = var.bucket
+    bucket  = aws_s3_bucket.this.bucket
     enabled = true
     prefix  = "access-logs"
   }
 
   connection_logs {
-    bucket  = var.bucket
+    bucket  = aws_s3_bucket.this.bucket
     enabled = true
     prefix  = "connection-logs"
+  }
+
+  tags = {
+    project = var.project_name
   }
 }
 
@@ -69,4 +118,27 @@ resource "aws_lb_listener" "this" {
 # Security Group
 ################################################################################
 
-#TODO - write the configuration for the alb module
+locals {
+  alb_security_group_name = "${local.alb_name}-sg"
+}
+
+resource "aws_security_group" "this" {
+  name   = local.alb_security_group_name
+  vpc_id = var.vpc_id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_all_inbound" {
+  security_group_id = aws_security_group.this.id
+  description       = "Allow traffic in from public internet"
+  from_port         = 80
+  to_port           = 80
+  ip_protocol       = "tcp"
+  cidr_ipv4         = "0.0.0.0/0"
+}
+
+resource "aws_vpc_security_group_egress_rule" "allow_all_outbound" {
+  security_group_id = aws_security_group.this.id
+  description       = "Send traffic out to the public internet"
+  ip_protocol       = "-1" # Allows all outbound traffic
+  cidr_ipv4         = "0.0.0.0/0"
+}
